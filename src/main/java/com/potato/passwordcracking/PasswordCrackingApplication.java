4deb3bd2f3095d73541888d83573ff10ea381e3a
aharris856 <abrahamfreemanharris@gmail.com>
2023-05-09 21:35:12 -0400

    fix to broken initial commit

--- src/main/java/com/potato/passwordcracking/PasswordCrackingApplication.java
+++ src/main/java/com/potato/passwordcracking/PasswordCrackingApplication.java
@@ -1,2 +1,9 @@
-package com.potato.passwordcracking;public class PasswordCrackingApplication {
+package com.potato.passwordcracking;
+
+public class PasswordCrackingApplication {
+
+    public static void main(String[] args) {
+        
+    }
+
 }
